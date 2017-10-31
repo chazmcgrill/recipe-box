ccdf2ecc14146b1ed173b934306c890685d89bd0
chazmcgrill <charlietaylordev@gmail.com>
2017-10-31 20:39:23 +0000

    Added Start Data
    Added starting data for the app. Fed data through to the recipes and created loops for each item and ingredient

--- client/index.js
+++ client/index.js
@@ -1,21 +1,43 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+let recipes = [
+  {
+    name: 'Macaroni Cheese',
+    method: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    ingredients: [ 'Cheese', 'Macaroni', 'Salt & Pepper']
+  },
+  {
+    name: 'Spaghetti Bolognese',
+    method: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    ingredients: [ 'Spaghetti', 'Minced Beef', 'Chopped Tomatoes']
+  },
+  {
+    name: 'Pizza',
+    method: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam.',
+    ingredients: [ 'Dough', 'Cheese', 'Tomato Puree']
+  }
+];
+
 class Recipe extends Component {
+  componentDidMount() {
+    console.log(this.props);
+  }
+
   render() {
     return (
-      <div className="list-item">Spag Bol
-        <div>
-          <p>Method</p>
-          <ol>
-            <li>Cook spag</li>
-            <li>Cook veg and meat</li>
-          </ol>
-          <p>Ingredients</p>
+      <div className="list-item">
+        <h4>{this.props.recipe.name}</h4>
+        <div className="hidden">
+          <strong>Method</strong>
+          <p>{this.props.recipe.method}</p>
+          <strong>Ingredients</strong>
           <ul>
-            <li>Spaghetti</li>
-            <li>Minced Beef</li>
-            <li>Chopped Tomatoes</li>
+            {this.props.recipe.ingredients.map(function(item, i) {
+              return (
+                <li>{item}</li>
+              )
+            })}
           </ul>
         </div>
       </div>
@@ -24,12 +46,21 @@ class Recipe extends Component {
 }
 
 class RecipeBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foundRecipes: this.props.recipeList
+    }
+  }
+
   render() {
     return (
       <div className="recipe-box">
-        <Recipe />
-        <div className="list-item">Macaroni Cheese</div>
-        <div className="list-item">Lasagne</div>
+          {this.props.recipeList.map(function(recipe, i) {
+            return (
+            <Recipe recipe={recipe} key={i}/>
+            )
+          })}
       </div>
     )
   }
@@ -46,14 +77,21 @@ class AddRecipe extends Component {
 }
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      foundRecipes: this.props.recipeList
+    };
+  }
+
   render() {
     return (
       <div className="container">
-        <RecipeBox />
+        <RecipeBox recipeList={this.state.foundRecipes}/>
         <AddRecipe />
       </div>
     )
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App recipeList={recipes}/>, document.getElementById('root'));
