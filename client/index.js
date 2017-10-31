a4e66052b0cff781ef63f3a0b24865449c3c5bd9
chazmcgrill <charlietaylordev@gmail.com>
2017-10-31 14:15:42 +0000

    Basic Styling React Skeleton
    Added basic styling and created a skeleton of required react components

--- client/index.js
+++ client/index.js
@@ -1,10 +1,57 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
+class Recipe extends Component {
+  render() {
+    return (
+      <div className="list-item">Spag Bol
+        <div>
+          <p>Method</p>
+          <ol>
+            <li>Cook spag</li>
+            <li>Cook veg and meat</li>
+          </ol>
+          <p>Ingredients</p>
+          <ul>
+            <li>Spaghetti</li>
+            <li>Minced Beef</li>
+            <li>Chopped Tomatoes</li>
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+
+class RecipeBox extends Component {
+  render() {
+    return (
+      <div className="recipe-box">
+        <Recipe />
+        <div className="list-item">Macaroni Cheese</div>
+        <div className="list-item">Lasagne</div>
+      </div>
+    )
+  }
+}
+
+class AddRecipe extends Component {
+  render() {
+    return (
+      <div>
+        <button>Add Recipe</button>
+      </div>
+    )
+  }
+}
+
 class App extends Component {
   render() {
     return (
-      <div>Working</div>
+      <div className="container">
+        <RecipeBox />
+        <AddRecipe />
+      </div>
     )
   }
 }
