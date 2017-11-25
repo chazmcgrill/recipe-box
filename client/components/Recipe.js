a260bde7a0090f10547552f82b42296578f372d4
chazmcgrill <charlietaylordev@gmail.com>
2017-11-25 13:25:28 +0000

    Show Info Click Handler
    Added click handlers to each recipe that toggles display of information

--- client/components/Recipe.js
+++ client/components/Recipe.js
@@ -1,15 +1,30 @@
 import React, { Component } from 'react';
 
 class Recipe extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showInfo: false
+    };
+    this.handleTitleClick = this.handleTitleClick.bind(this);
+  }
+
+  handleTitleClick() {
+    const showInfo = !this.state.showInfo;
+    this.setState({showInfo})
+  }
+
   render() {
     const {name, description, id} = this.props.recipe;
+    const {showInfo} = this.state;
     const toppings = this.props.recipe.toppings.map((item, i) => (
       <li key={i}>{item}</li>
-    ))
+    ));
+
     return (
       <div className="list-item" >
-        <h4>{name}</h4>
-        <div>
+        <h4 onClick={this.handleTitleClick}>{name}</h4>
+        {showInfo ? <div>
           <p>{description}</p>
           <strong>Toppings</strong>
           <ul>
@@ -17,7 +32,7 @@ class Recipe extends Component {
           </ul>
           <button className="danger" onClick={() => this.props.onDelete(id)}>Delete</button>
           <button onClick={() => this.props.onEdit(id)}>Edit</button>
-        </div>
+        </div> : null}
       </div>
 
     )
